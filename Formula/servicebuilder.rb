b332daee84a57d101c2b839daae49dafbd8d4b17
goreleaserbot <goreleaser@carlosbecker.com>
2020-07-03 12:14:35 +0530

    Brew formula update for servicebuilder version v0.9.6

--- Formula/servicebuilder.rb
+++ Formula/servicebuilder.rb
@@ -2,9 +2,9 @@
 class Servicebuilder < Formula
   desc "A gRPC based Micro Service scaffolding in GoLang with HTTP/JSON gRPC gateway"
   homepage "https://cnative.dev"
-  url "https://github.com/cnative/servicebuilder/releases/download/v0.9.5/servicebuilder_darwin_amd64.tar.gz"
-  version "0.9.5"
-  sha256 "bb9ce7b0d54229ea1378c918c1802542f67345bc0ca4cfa204a4abf583bd08de"
+  url "https://github.com/cnative/servicebuilder/releases/download/v0.9.6/servicebuilder_darwin_amd64.tar.gz"
+  version "0.9.6"
+  sha256 "a3c1ee8be315532bf591b029a5fcb83ec6bed76b8ba3ad5c6156b70cdf2e5ef3"
 
   def install
     bin.install "servicebuilder"
